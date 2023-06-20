c1acc1961472dcd3e0807d57385512da7c9e2c0d
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2023-06-20 10:38:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(["/", "/:name"], (req, res) => {
+  const greeting = "<h1>Hello From Node on Fly!</h1>";
+  const name = req.params["name"];
+  if (name) {
+    res.send(greeting + "</br>and hello to " + name);
+  } else {
+    res.send(greeting);
+  }
+});
+
+app.use(express.json())
+
+app.post('/', function (req, res, next) {
+  console.log(req.body)
+  res.json(req.body)
+})
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
