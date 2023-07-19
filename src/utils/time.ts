0ceeb7eb77e2595be087af9f5be993485e4cb12b
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2023-07-19 09:46:47 +0200

    Show exact number of seconds for less or equal 5 minutes of duration

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,7 +3,7 @@ export const formatTime = (duration: number) => {
   const minutes = Math.floor((duration - hours * 60 * 60) / 60);
   const seconds = Math.floor(duration - minutes * 60);
   // Show exact number of seconds for less or equal 5 minutes of duration
-  const showSeconds = seconds <= 5;
+  const showSeconds = minutes <= 5;
 
   return `${hours}:${minutes}:${showSeconds ? seconds : 0}`;
 };
